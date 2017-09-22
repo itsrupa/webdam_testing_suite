dd6da14990d3b73bc7f659871cb80177f8bde31b
Rupa <rupa@Rupas-MBP.hsd1.ca.comcast.net>
2017-09-22 11:16:46 -0700

    updated logout tests with additional test

--- test/logoutTests.js
+++ test/logoutTests.js
@@ -60,3 +60,68 @@ describe("Successful Log Out", function(){
 	});
 
 });
+
+describe("Retrieve Assets after Log Out", function(){
+	var body;
+	var res;
+	var access_token;
+
+	before(function(done) {
+		request.post({ url: config["baseUrl"] + "/oauth/token", 
+			form: {
+				"grant_type": "client_credentials",
+				"client_id": config["client_id"],
+				"client_secret": config["client_secret"],
+				"scope": "*"
+			},
+			json: true
+		},
+		function(error, r, b){
+			access_token = b.access_token;
+			request.get({ url: config["baseUrl"] + "/api/v1/login", 
+				headers: {
+					Authorization: access_token
+				},
+				json: true
+			},
+			function(error, r, b){
+				request.get({ url: config["baseUrl"] + "/api/v1/logout", 
+					headers: {
+						Authorization: access_token
+					},
+					json: true
+				},
+				function(error, r, b){
+					res = r;
+					body = b;
+					request.get({ url: config["baseUrl"] + "/api/v1/asset/12345", 
+						headers: {
+							Authorization: access_token
+						},
+						json: true
+					},
+					function(error, r, b){
+						res = r;
+						body = b;
+						done();
+					});
+				});
+			});
+		});
+	});
+
+	it("has valid response code in the response", function(done) {
+		assert.exists(res.statusCode, "Did not find the Status Code in the response");
+		assert(res.statusCode == 200, "Did not receive the 200 OK as the status code");
+		done();
+
+	});
+
+
+	it("has returned no results", function(done) {
+		assert(body.length == 0, "Did not receive the expected number of Assets in the response");
+		done();
+
+	});
+
+});
